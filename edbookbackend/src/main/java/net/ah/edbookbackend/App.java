4e2eaf4475ee933bc7cb8c06d3b7c0486e494ca8
Supun Aravinda <aravinda.airhorse@gmail.com>
2018-01-24 11:41:41 +0530

    commit startup settings

---
+++ edbookbackend/src/main/java/net/ah/edbookbackend/App.java
@@ -0,0 +1,13 @@
+package net.ah.edbookbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
